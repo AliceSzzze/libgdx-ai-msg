5ab9cd931eb2729f5da38a85835dd497467b9539
SimonK5 <5222sak@gmail.com>
2023-12-15 22:40:33 -0500

    shortened HelloApp.cpp top of file comment

--- c++/HelloApp.cpp
+++ c++/HelloApp.cpp
@@ -1,33 +1,5 @@
-//
-//  HelloApp.cpp
-//  Cornell University Game Library (CUGL)
-//
-//  This is the implementation file for the custom application. This is the
-//  definition of your root (and in this case only) class.
-//
-//  CUGL zlib License:
-//      This software is provided 'as-is', without any express or implied
-//      warranty.  In no event will the authors be held liable for any damages
-//      arising from the use of this software.
-//
-//      Permission is granted to anyone to use this software for any purpose,
-//      including commercial applications, and to alter it and redistribute it
-//      freely, subject to the following restrictions:
-//
-//      1. The origin of this software must not be misrepresented; you must not
-//      claim that you wrote the original software. If you use this software
-//      in a product, an acknowledgment in the product documentation would be
-//      appreciated but is not required.
-//
-//      2. Altered source versions must be plainly marked as such, and must not
-//      be misrepresented as being the original software.
-//
-//      3. This notice may not be removed or altered from any source distribution.
-//
-//  Author: Walker White
-//  Version: 1/8/17
-//
-// Include the class header, which includes all of the CUGL classes
+// Altered HelloApp.cpp. Should not be added to CUGL
+
 #include "HelloApp.h"
 #include <cugl/base/CUBase.h>
 #include "rtree.h"
